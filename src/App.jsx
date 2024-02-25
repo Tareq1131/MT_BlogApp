cb0976b6e923ca89100594447862d71d36c452e2
Tareq1131 <tareqislam076@gmail.com>
2024-02-25 18:56:42 +0600

    added api

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,40 @@
 /* eslint-disable no-undef */
 
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import authService from "./appwrite/auth";
-import { login, logout } from "./store/authSlice";
-import "./App.css";
-import { Footer, Header } from "./components";
-import { Outlet } from "react-router-dom";
+import  { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
-
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   useEffect(() => {
-    authService
-      .getCurrentUser()
-      .then((userData) => {
-        if (userData) {
-          dispatch(login({ userData }));
-        } else {
-          dispatch(logout());
-        }
-      })
-      .finally(() => setLoading(false));
-  }, [dispatch]);
-
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
   return !loading ? (
-    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
-      <div className="w-full block">
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
         <Header />
         <main>
-          TODO: <Outlet />
+        TODO:  <Outlet />
         </main>
         <Footer />
       </div>
     </div>
-  ) : null;
+  ) : null
 }
 
-export default App;
+export default App
